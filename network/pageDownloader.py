2c0218d540b6b7d45aa097f9e6cbb5a744c0aba0
mile hashemi nejhad <mlddddd619@gmail.com>
2021-09-27 01:36:52 -0700

    init for first time

---
+++ network/pageDownloader.py
@@ -0,0 +1,7 @@
+def take_content(url):
+    import requests
+    import ssl 
+    requests.packages.urllib3.util.ssl_.DEFAULT_CIPHERS = "TLS13-CHACHA20-POLY1305-SHA256:TLS13-AES-128-GCM-SHA256:TLS13-AES-256-GCM-SHA384:ECDHE:!COMPLEMENTOFDEFAULT"
+    headers = {'user-agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36"}
+    page = requests.get(url, headers=headers, verify=ssl.CERT_NONE)
+    return page.content
---
+++ scrapper/CBCommentScrapper.py
@@ -0,0 +1,37 @@
+from Include.network import pageDownloader as pd
+import re
+from bs4 import BeautifulSoup as bs
+
+dummyUrl = 'https://castbox.fm/episode/SERIAL-KILLER%3A-Bruce-McArthur-id1275406-id399043533?country=us'
+
+
+class CommentExt:
+    _CleanComments = []
+
+    def _remove_html_tags(self, text):
+        return re.sub(re.compile('<.*?>'), '', text)
+
+    def mainTextExt(self, url):
+        self._CleanComments = []
+        mainPageParsed = bs(pd.take_content(url), 'html.parser')
+        Comments = mainPageParsed.select('div p', {'class': 'commentItemDes'})
+        for i in Comments:
+            Comment = Comments.pop()
+            if "commentItemDes" == Comment.get("class")[0]:
+                self._CleanComments.append(self._remove_html_tags(str(Comment)))
+        return self._CleanComments
+
+    def episodeCommentTextExt(self, epUrl):
+        self._CleanComments = []
+        self.content = pd.take_content(epUrl)
+        epParsedForComment = bs(self.content, 'html.parser')
+        Comments = epParsedForComment.select('p.commentItemDes')
+        print(Comments)
+        for Comment in Comments:
+            if "commentItemDes" == Comment.get("class")[0]:
+                self._CleanComments.append(self._remove_html_tags(str(Comment)))
+        return self._CleanComments
+
+
+if __name__ == '__main__':
+    print(CommentExt().episodeCommentTextExt(dummyUrl))
